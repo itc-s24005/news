f6e0ef53311afc31bcc870b41de88694694a01f0
itc-s24005 <s24005@std.it-college.ac.jp>
2025-12-10 13:50:29 +0900

    news

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { NewsItem } from "./types";
+import Image from "next/image";
 
 export default async function Page() {
   const apiKey = process.env.NEWSDATA_API_KEY!;
@@ -14,8 +15,9 @@ export default async function Page() {
       <h1 style={{fontSize: "40px"}}>最新ニュース</h1>
 
       {newsList.map((news) => (
-        <div key={news.link} style={{padding: "15px"}}>
-          <a href={news.link}>
+        <div key={news.link} style={{padding: "20px 50px", margin: "15px"}}>
+          <a href={news.link} style={{padding: "40px"}}>
+          <img src={news.image_url} style={{width: "300px"}}/>
           <h2 style={{fontSize: "25px"}}>{news.title}</h2>
           <p>{news.description}...</p>
           </a>
